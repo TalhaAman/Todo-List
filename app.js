89b1cf9e58c621941efa6c7db7404b2c72b5be51
Talha Aman <talha.aman.bajwa@gmail.com>
2022-09-21 01:40:51 +0500

    changed code from innerHTML method to create Element method

--- app.js
+++ app.js
@@ -3,23 +3,47 @@ const todoList = document.querySelector('.todo-list');
 const addForm = document.querySelector('.add');
 
 // Adding new Todos to the list
-const addingTodo = addedText => {
-  todoList.innerHTML +=
-  `<li class="list-item">
-    <span>${addedText}</span>
-    <i class="far fa-trash-can delete"></i>
-  </li>`
-}
+// const addingTodo = addedText => {
+//   todoList.innerHTML +=
+//   `<li class="list-item">
+//     <span>${addedText}</span>
+//     <i class="far fa-trash-can delete"></i>
+//   </li>`
+// }
+// addForm.addEventListener('submit', e => {
+//   e.preventDefault();
+//   const addedText = addForm.add.value.trim();
+//   if(addedText.length){
+//   addingTodo(addedText);
+//   addForm.reset();
+//   }
+// })
+
+// // Deleting Todo-items by Clicking on Bin icon
+// todoList.addEventListener('click', e => {
+//   if(e.target.classList.contains('delete')){
+//     e.target.parentElement.remove();
+//   }
+// })
+
+
 addForm.addEventListener('submit', e => {
   e.preventDefault();
-  const addedText = addForm.add.value.trim();
-  if(addedText.length){
-  addingTodo(addedText);
-  addForm.reset();
-  }
-})
+const li = document.createElement('li');
+li.classList.add('list-item');
 
-// Deleting Todo-items by Clicking on Bin icon
+const addedText = addForm.add.value.trim();
+const text = document.createTextNode(addedText);
+li.appendChild(text);
+
+const bin =document.createElement('i');
+bin.classList.add('far','fa-trash-can','delete');
+li.appendChild(bin);
+if(text.length){
+  todoList.insertBefore(li, todoList.childNodes[2]);
+  addForm.reset();
+}
+})
 todoList.addEventListener('click', e => {
   if(e.target.classList.contains('delete')){
     e.target.parentElement.remove();
