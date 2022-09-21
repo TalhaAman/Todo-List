8e4ca2c682979325369bba735bcd1a528e302df0
Talha Aman <talha.aman.bajwa@gmail.com>
2022-09-22 00:25:35 +0500

    Added Search Bar

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// const searchBar = document.querySelector('.search-bar input');
+const searchBar = document.querySelector('.search-bar input');
+const searchForm = document.querySelector('.search-bar')
 const todoList = document.querySelector('.todo-list');
 const addForm = document.querySelector('.add');
 
@@ -30,7 +31,7 @@ const addForm = document.querySelector('.add');
 addForm.addEventListener('submit', e => {
   e.preventDefault();
 const li = document.createElement('li');
-li.classList.add('list-item');
+// li.classList.add('list-item');
 
 const addedText = addForm.add.value.trim();
 const text = document.createTextNode(addedText);
@@ -44,8 +45,32 @@ if(text.length){
   addForm.reset();
 }
 })
+
+// Deleting Todo-items by Clicking on Bin icon
 todoList.addEventListener('click', e => {
   if(e.target.classList.contains('delete')){
     e.target.parentElement.remove();
   }
+})
+
+//Search-Bar
+
+const searchedItems = searchTerm => {
+  Array.from(todoList.children)
+  .map(term => {
+if(!term.textContent.toLowerCase().includes(searchTerm)){
+  term.classList.add('filtered');
+} else {
+  term.classList.remove('filtered');
+}
+})
+}
+
+searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+})
+searchBar.addEventListener('keyup', () => {
+  const searchTerm = searchBar.value.trim().toLowerCase();
+  searchedItems(searchTerm);
+
 })
